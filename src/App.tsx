3b540cd17dd8ee8f7fcbbc1ed0267e44733b966c
Ayush Rajniwal <ayush.rajniwal@gmail.com>
2021-11-21 18:09:45 +0530

    Touch Slider Added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TouchSlider from "./components/TouchSlider/TouchSlider";
+
+function App() {
+	return (
+		<div className="background center">
+			<TouchSlider></TouchSlider>
+		</div>
+	);
+}
+
+export default App;
